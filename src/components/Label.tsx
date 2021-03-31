98fc9e9f966e7816e7e185a9a0732b40f1284e91
pawgog <pgogola10@gmail.com>
2021-03-31 21:18:25 +0200

    Set and style label component.

--- src/components/Label.tsx
+++ src/components/Label.tsx
@@ -1,13 +1,23 @@
+import { format, fromUnixTime } from 'date-fns'
 import { LabelProps } from '../types/WeatherAppTypes';
+import { LabelStyled, SunIconStyle } from '../style/Label.style';
+import { ReactComponent as Sunrise } from '../assets/sunrise.svg';
+import { ReactComponent as Sunset } from '../assets/sunset.svg';
 
 function Label({ cityDetails } : LabelProps) {
   const { city } = cityDetails;
   console.log(city);
   
   return (
-      <div className="weather-app__label">
-        Label component {city.name}
-      </div>
+      <LabelStyled>
+        {city.name} 
+        <SunIconStyle>
+          <Sunrise />
+            <span>{format(fromUnixTime(city.sunrise), 'HH:mm')}</span>
+          <Sunset />
+            <span>{format(fromUnixTime(city.sunset), 'HH:mm')}</span>
+        </SunIconStyle>
+      </LabelStyled>
     );
   }
 
---
+++ src/style/Label.style.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const LabelStyled = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  padding: 1.3rem;
+`
+
+export const SunIconStyle = styled.span`
+  display: flex;
+  align-items: center;
+  width: 240px;
+  & span {
+    min-width: 75px;
+  }
+`
