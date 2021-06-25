c065f61bb87315d69c541a55b3d94e120f1a75a1
pawgog <pgogola10@gmail.com>
2021-06-25 20:08:33 +0200

    Add react component.

---
+++ src/components/TemperatureChart.tsx
@@ -0,0 +1,12 @@
+import { Line } from 'react-chartjs-2';
+
+function TemperatureChart() {
+    return (
+      <div>
+        <div>Chart</div>
+      </div>
+    );
+  }
+};
+
+export default TemperatureChart;
