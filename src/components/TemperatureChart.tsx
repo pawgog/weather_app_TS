10c5a3dc68bda9d4dda7958588fac9427469d3cc
pawgog <pgogola10@gmail.com>
2021-06-30 22:48:19 +0200

    Set the state of data.

--- src/components/TemperatureChart.tsx
+++ src/components/TemperatureChart.tsx
@@ -2,6 +2,19 @@
 import { WeatherChart } from '../types/WeatherAppTypes';
 
 function TemperatureChart({ weatherList }: WeatherChart) {
+  const state = {
+    labels: [],
+    datasets: [
+      {
+        label: 'Temperature',
+        backgroundColor: 'rgba(75,192,192,1)',
+        borderColor: 'rgba(0,0,0,1)',
+        borderWidth: 2,
+        data: []
+      }
+    ]
+}
+
   return (
     <>
       <div>Chart</div>
