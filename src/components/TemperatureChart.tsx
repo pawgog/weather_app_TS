d3e4f787cbb8e52faa6110aaced763b3a541b28f
pawgog <pgogola10@gmail.com>
2021-07-01 23:20:55 +0200

    Display line chart.

--- src/components/TemperatureChart.tsx
+++ src/components/TemperatureChart.tsx
@@ -1,7 +1,9 @@
-// import { Line } from 'react-chartjs-2';
 import { WeatherChart } from '../types/WeatherAppTypes';
+import { Line } from 'react-chartjs-2';
 
 function TemperatureChart({ weatherList }: WeatherChart) {
+  // console.log(weatherList);
+  
   const state = {
     labels: [],
     datasets: [
@@ -13,12 +15,15 @@ function TemperatureChart({ weatherList }: WeatherChart) {
         data: []
       }
     ]
-}
+  }
 
   return (
-    <>
-      <div>Chart</div>
-    </>
+    <div>
+        <Line
+          data={state}
+          type='line'
+        />
+    </div>
   );
 }
 
