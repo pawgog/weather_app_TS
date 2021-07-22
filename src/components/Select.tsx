2a11cf0aeccbf651bc22145228cdf721d2350b63
pawgog <pgogola10@gmail.com>
2021-07-22 23:24:07 +0200

    Select tag style.

--- src/components/Select.tsx
+++ src/components/Select.tsx
@@ -3,7 +3,6 @@ import { SelectStyled } from '../style/Select.style';
 function Select() {
   return (
     <SelectStyled>
-      <label htmlFor="weather">Change weather details:</label>
       <select name="weather" id="weather">
         <option value="temp">Temperature</option>
         <option value="humidity">Humidity</option>
--- src/style/Select.style.ts
+++ src/style/Select.style.ts
@@ -1,5 +1,8 @@
 import styled from 'styled-components';
 
 export const SelectStyled = styled.div`
-
+  display: flex;
+  justify-content: flex-end;
+  align-items: flex-end;
+  margin: 12px 0;
 `
